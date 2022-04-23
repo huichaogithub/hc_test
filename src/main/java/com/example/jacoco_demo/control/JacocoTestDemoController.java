fbdd7a19f4c61858a1c687ee5c7f399761781a5c
huichao <huichao_0102@163.com>
2022-04-23 14:45:11 +0800

    first commit

---
+++ src/main/java/com/example/jacoco_demo/control/JacocoTestDemoController.java
@@ -0,0 +1,14 @@
+package com.example.jacoco_demo.control;
+
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller
+public class JacocoTestDemoController {
+
+
+    public void tets(){
+        System.out.println(1234);
+    }
+}
